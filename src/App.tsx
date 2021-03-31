115a7a785553f3af402979c90650d2fdd8864f86
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-03-31 21:26:29 +0700

    feat(app): adding external font and reset css

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,36 @@ import StartPage from '@/pages/Start'
 import LoginPage from '@/pages/Login'
 import RegisterPage from '@/pages/Register'
 import ResetPage from '@/pages/Reset'
+import styled from 'styled-components'
+import './theme/reset.css'
+
+const RootStyles = styled.div`
+  * {
+    color: ${(props) => props.theme.color.dark};
+    font-family: 'Hind Siliguri', sans-serif;
+    font-size: 14px;
+  }
+`
 
 const App = (): ReactElement => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact>
-        <StartPage />
-      </Route>
-      <Route path="/login" exact>
-        <LoginPage />
-      </Route>
-      <Route path="/register" exact>
-        <RegisterPage />
-      </Route>
-      <Route path="/reset" exact>
-        <ResetPage />
-      </Route>
-    </Switch>
-  </BrowserRouter>
+  <RootStyles>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <StartPage />
+        </Route>
+        <Route path="/login" exact>
+          <LoginPage />
+        </Route>
+        <Route path="/register" exact>
+          <RegisterPage />
+        </Route>
+        <Route path="/reset" exact>
+          <ResetPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </RootStyles>
 )
 
 export default App
