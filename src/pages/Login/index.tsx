82e13dc2f84bfd239c5cf14a3e90251e1d701897
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-04-01 00:16:36 +0700

    feat(pages/login): adding state for each input

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,49 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import Input from '@/components/form/Input'
 import Button from '@/components/form/Button'
 import { useHistory } from 'react-router-dom'
+import { InputState } from 'Types'
 import AuthLayout, {
   AuthFooterLabel,
   AuthHeading,
   AuthInputGroup,
 } from '@/layout/AuthLayout'
+import initialInputState from '@/initials/initialInputState'
 
 export default function index(): ReactElement {
   const history = useHistory()
 
   const toRegisterPage = () => history.push('/register')
   const toResetPage = () => history.push('/reset')
+
+  const [inputEmail, setInputEmail] = useState<InputState<string>>(
+    initialInputState
+  )
+  const [inputPassword, setInputPassword] = useState<InputState<string>>(
+    initialInputState
+  )
+
+  const inputEmailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputEmail({
+      ...inputEmail,
+      value: e.target.value,
+    })
+  }
+
+  const inputPasswordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputPassword({
+      ...inputPassword,
+      value: e.target.value,
+    })
+  }
+
+  const submitHandler = () => {
+    const payload = {
+      email: inputEmail.value,
+      password: inputPassword.value,
+    }
+    alert(JSON.stringify(payload, null, 2))
+  }
 
   return (
     <AuthLayout style={{ paddingBottom: '76px' }}>
@@ -23,12 +54,14 @@ export default function index(): ReactElement {
           fullWidth
           placeholder="Email"
           type="email"
+          onChange={inputEmailHandler}
         />
         <Input
           icon="/lock--gray.svg"
           fullWidth
           placeholder="Password"
           type="password"
+          onChange={inputPasswordHandler}
         />
       </AuthInputGroup>
       <AuthFooterLabel
@@ -37,7 +70,11 @@ export default function index(): ReactElement {
       >
         Lupa Password ?
       </AuthFooterLabel>
-      <Button variant="auth" style={{ marginTop: '86px' }}>
+      <Button
+        variant="auth"
+        style={{ marginTop: '86px' }}
+        onClick={submitHandler}
+      >
         Login
       </Button>
       <AuthFooterLabel>
