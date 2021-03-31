c1d49939fd9516856b444491bc8e64cf27f4fe9b
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-03-31 23:37:40 +0700

    feat(pages): adding design for login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,30 +1,55 @@
 import React, { ReactElement } from 'react'
 import Input from '@/components/form/Input'
 import Button from '@/components/form/Button'
-import AuthLayout from '@/layout/AuthLayout'
+import { useHistory } from 'react-router-dom'
+import AuthLayout, {
+  AuthFooterLabel,
+  AuthHeading,
+  AuthInputGroup,
+} from '@/layout/AuthLayout'
 
 export default function index(): ReactElement {
+  const history = useHistory()
+
+  const toRegisterPage = () => history.push('/register')
+  const toResetPage = () => history.push('/reset')
+
   return (
-    <AuthLayout>
-      <Input
-        placeholder="hello"
-        icon="https://res.cloudinary.com/alfianandidev/image/upload/web/javascript.svg"
-      />
-      <Button
-        icon="https://res.cloudinary.com/alfianandidev/image/upload/web/javascript.svg"
-        variant="primary"
+    <AuthLayout style={{ paddingBottom: '76px' }}>
+      <AuthHeading style={{ marginTop: '148px' }}>Login App</AuthHeading>
+      <AuthInputGroup>
+        <Input
+          icon="/email--gray.svg"
+          fullWidth
+          placeholder="Email"
+          type="email"
+        />
+        <Input
+          icon="/lock--gray.svg"
+          fullWidth
+          placeholder="Password"
+          type="password"
+        />
+      </AuthInputGroup>
+      <AuthFooterLabel
+        onClick={toResetPage}
+        style={{ color: 'white', textAlign: 'left', cursor: 'pointer' }}
       >
-        Hello
+        Lupa Password ?
+      </AuthFooterLabel>
+      <Button variant="auth" style={{ marginTop: '86px' }}>
+        Login
       </Button>
-      <Button
-        icon="https://res.cloudinary.com/alfianandidev/image/upload/web/javascript.svg"
-        variant="secondary"
-      >
-        Hello
-      </Button>
-      <Button variant="auth" fullWidth style={{ textAlign: 'center' }}>
-        Hello
-      </Button>
+      <AuthFooterLabel>
+        <span>Belum punya akun ? </span>
+        <b
+          onClick={toRegisterPage}
+          onKeyDown={toRegisterPage}
+          aria-hidden="true"
+        >
+          Register
+        </b>
+      </AuthFooterLabel>
     </AuthLayout>
   )
 }
