a157e3a0ec851881e46e0316ffd4f19f7c0ed2e4
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-03-31 17:59:54 +0700

    feat(app): init auth route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
 import React, { ReactElement } from 'react'
-import Button from '@/components/Button'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import StartPage from '@/pages/Start'
+import LoginPage from '@/pages/Login'
+import RegisterPage from '@/pages/Register'
+import ResetPage from '@/pages/Reset'
 
-const App = (): ReactElement => <Button variant="hi">Click me</Button>
+const App = (): ReactElement => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact>
+        <StartPage />
+      </Route>
+      <Route path="/login" exact>
+        <LoginPage />
+      </Route>
+      <Route path="/register" exact>
+        <RegisterPage />
+      </Route>
+      <Route path="/reset" exact>
+        <ResetPage />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+)
 
 export default App
