3818057b71b4cc6ee6b3b50215c9c9ec8accb851
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-04-01 12:20:48 +0700

    feat(form/input): adding error message handler and component

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -4,7 +4,10 @@ import styled from 'styled-components'
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   icon?: string
   fullWidth?: boolean
+  errorMessage?: string
 }
+
+interface InputErrorMessageProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 interface InputProps {
   fullWidth?: boolean
@@ -12,6 +15,11 @@ interface InputProps {
 
 const Root = styled.div`
   position: relative;
+`
+
+const InputContainer = styled.div`
+  position: relative;
+  z-index: 1;
 `
 
 const Input = styled.input<InputProps>`
@@ -38,16 +46,37 @@ const ImageIcon = styled.img`
   z-index: 1;
 `
 
+const RootErrorMessage = styled.div`
+  background-color: ${(props) => props.theme.color.danger};
+  color: white !important;
+  padding: 8px;
+  padding-top: 13px;
+  border-radius: 0 0 5px 5px;
+  position: relative;
+  top: -5px;
+`
+
+const InputErrorMessage = (props: InputErrorMessageProps): ReactElement => (
+  <RootErrorMessage>{props.children}</RootErrorMessage>
+)
+
 export default function index(props: Props): ReactElement {
   return (
     <Root>
-      {props.icon && <ImageIcon alt="icon" src={props.icon} />}
-      <Input
-        {...props}
-        style={{
-          paddingLeft: props.icon ? '48px' : '16px',
-        }}
-      />
+      <InputContainer>
+        {props.icon && <ImageIcon alt="icon" src={props.icon} />}
+        <Input
+          {...props}
+          style={{
+            paddingLeft: props.icon ? '48px' : '16px',
+          }}
+        />
+      </InputContainer>
+      {props.errorMessage && (
+        <InputErrorMessage>{props.errorMessage}</InputErrorMessage>
+      )}
     </Root>
   )
 }
+
+export { InputErrorMessage }
