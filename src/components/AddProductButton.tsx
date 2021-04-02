49550ec2ee8d4659a033b43e41bc52f9deb574a9
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-04-02 19:02:04 +0700

    feat(components): addProductButton.tsx - adding validation

--- src/components/AddProductButton.tsx
+++ src/components/AddProductButton.tsx
@@ -3,6 +3,9 @@ import Button from '@/components/form/Button'
 import Input from '@/components/form/Input'
 import Modal, { ModalTitle, ModalContent } from '@/components/Modal'
 import { initialProducts } from '@/initials/initialProduct'
+import { InputState } from 'Types'
+import initialInputState from '@/initials/initialInputState'
+import { EMPTY_VALUE_MESSAGE } from '@/validation/form'
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
   payloadHandler: (payload: string) => void
@@ -10,21 +13,34 @@ interface Props extends React.HTMLAttributes<HTMLElement> {
 
 export default function AddProductButtonGroup(props: Props): ReactElement {
   const [isModalAddProductShow, setIsModalAddProductShow] = useState(false)
-  const [productName, setProductName] = useState<string>('')
+  const [productName, setProductName] = useState<InputState<string>>(
+    initialInputState
+  )
   const [productQuantity, setProductQuantity] = useState<string>('')
 
   const toggleModalAddProduct = () => {
-    setProductName('')
+    setProductName(initialInputState)
+    setProductQuantity('')
     setIsModalAddProductShow(!isModalAddProductShow)
   }
 
   const sendPayload = () => {
+    if (!productName.value) {
+      setProductName({
+        ...productName,
+        errorMessage: EMPTY_VALUE_MESSAGE,
+      })
+      return
+    }
     toggleModalAddProduct()
-    props.payloadHandler(`${productQuantity} ${productName}`)
+    props.payloadHandler(`${productQuantity} ${productName.value}`)
   }
 
   const inputProductNameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setProductName(e.target.value)
+    setProductName({
+      value: e.target.value,
+      errorMessage: '',
+    })
   }
 
   const inputProductQuantityHandler = (
@@ -53,9 +69,10 @@ export default function AddProductButtonGroup(props: Props): ReactElement {
               <ModalContent>
                 <Input
                   list="products"
-                  placeholder="Pilih produk"
+                  placeholder="Pilih / input produk"
                   fullWidth
                   onChange={inputProductNameHandler}
+                  errorMessage={productName.errorMessage}
                 />
                 <datalist id="products">
                   {initialProducts.map((product) => (
@@ -68,7 +85,7 @@ export default function AddProductButtonGroup(props: Props): ReactElement {
               <ModalContent>
                 <Input
                   fullWidth
-                  placeholder="Jumlah"
+                  placeholder="Jumlah. Misal : 5 kg, 2 liter, atau 3"
                   onChange={inputProductQuantityHandler}
                 />
               </ModalContent>
