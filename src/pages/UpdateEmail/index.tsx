1af507a4edbe418101a45387722c16e703e5a3d9
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-04-06 00:34:07 +0700

    feat(edit-email): adding feature to edit user email

--- src/pages/UpdateEmail/index.tsx
+++ src/pages/UpdateEmail/index.tsx
@@ -5,26 +5,82 @@ import MainLayout, {
   ProfileButtonGroup,
   ProfileInputGroup,
 } from '@/layout/MainLayout'
-import React, { ReactElement } from 'react'
+import { userAtom } from '@/store/userAtom'
+import { useAtom } from 'jotai'
+import React, { ReactElement, useState } from 'react'
+import firebase from '@/utils/Firebase'
+import { User } from 'Types'
+import { editUser } from '@/http/User'
 
 export default function index(): ReactElement {
+  const [user, setUser] = useAtom(userAtom)
+  const [newEmail, setNewEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isRequestEditEmail, setIsRequestEditEmail] = useState(false)
+
+  const inputNewEmailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewEmail(e.target.value)
+  }
+
+  const inputPasswordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }
+
+  const submitEditEmailHandler = () => {
+    if (!user) return
+    setIsRequestEditEmail(true)
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(user.email, password)
+      .then((credential) => {
+        credential.user?.updateEmail(newEmail).then(() => {
+          const newUser: User = {
+            ...user,
+            email: newEmail,
+          }
+          editUser(newUser).then(() => {
+            setUser(newUser)
+            setIsRequestEditEmail(false)
+            setNewEmail('')
+            setPassword('')
+          })
+          console.log('update email success')
+        })
+      })
+  }
+
   return (
     <MainLayout>
       <HeadingLayout>Ubah Email</HeadingLayout>
       <ProfileInputGroup style={{ marginBottom: '24px' }}>
-        <Input fullWidth icon="email--gray.svg" value="alfian@andi.com" />
+        <Input fullWidth icon="email--gray.svg" value={user?.email} readOnly />
       </ProfileInputGroup>
       <p style={{ marginBottom: '16px' }}>Email Baru</p>
       <ProfileInputGroup>
-        <Input fullWidth icon="email--gray.svg" placeholder="Email baru" />
+        <Input
+          fullWidth
+          icon="email--gray.svg"
+          placeholder="Email baru"
+          value={newEmail}
+          onChange={inputNewEmailHandler}
+        />
         <Input
           fullWidth
           icon="lock--gray.svg"
           placeholder="Password untuk konfirmasi"
+          value={password}
+          onChange={inputPasswordHandler}
+          type="password"
         />
       </ProfileInputGroup>
       <ProfileButtonGroup>
-        <Button variant="primary" align="center" fullWidth>
+        <Button
+          variant="primary"
+          align="center"
+          fullWidth
+          onClick={submitEditEmailHandler}
+          isLoading={isRequestEditEmail}
+        >
           Ubah Email
         </Button>
       </ProfileButtonGroup>
