e276a43fa342bcbdfbdd3c959712ea644fea363b
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-04-09 13:33:49 +0700

    refactor(services): remove service folder

--- src/services/FirebaseTimestamp.ts
+++
@@ -1,6 +0,0 @@
-import Firebase from 'firebase'
-
-const generateFirebaseTimestampNow = (): Firebase.firestore.Timestamp =>
-  new Firebase.firestore.Timestamp(new Date().getTime() / 1000, 0)
-
-export { generateFirebaseTimestampNow }
