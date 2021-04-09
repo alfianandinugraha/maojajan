0bdc97607f05ba52548f83a90e1fd18715de8433
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-04-09 11:19:42 +0700

    feat(utils): date.ts to format all about date

---
+++ src/utils/Date.ts
@@ -0,0 +1,50 @@
+import firebase from 'firebase'
+
+const indonesianMonth = [
+  'Januari',
+  'Februari',
+  'Maret',
+  'April',
+  'Mei',
+  'Juni',
+  'Juli',
+  'Agustus',
+  'September',
+  'Oktober',
+  'November',
+  'Desember',
+]
+
+const appendZero = (val: number): string =>
+  val < 10 ? `0${val}` : val.toString()
+
+const DateToInputValueHTML = (date: Date): string => {
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const year = date.getFullYear()
+  return `${year}-${appendZero(month)}-${appendZero(day)}`
+}
+
+const HeadingDateFormat = (
+  firebaseDate: firebase.firestore.Timestamp
+): string => {
+  const date = firebaseDate.toDate()
+  const month = date.getMonth()
+  const day = date.getDate()
+  const year = date.getFullYear()
+  const format = `${day} ${indonesianMonth[month]} ${year}`
+  return format
+}
+
+const generateFirebaseTimestampNow = (): firebase.firestore.Timestamp =>
+  new firebase.firestore.Timestamp(new Date().getTime() / 1000, 0)
+
+const DateToFirebase = (date: Date): firebase.firestore.Timestamp =>
+  new firebase.firestore.Timestamp(date.getTime() / 1000, 0)
+
+export {
+  DateToInputValueHTML,
+  HeadingDateFormat,
+  generateFirebaseTimestampNow,
+  DateToFirebase,
+}
